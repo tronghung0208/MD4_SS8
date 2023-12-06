b9c27017c8b6d2deb6a5cf988e512a53643b5655
tronghung0208 <phanhung0208ytna@gmail.com>
2023-12-06 16:03:53 +0700

    SS8

---
+++ src/main/java/com/example/ss8/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.ss8.service;
+
+import com.example.ss8.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserService {
+    public void insertUser(User user) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User user) throws SQLException;
+}
